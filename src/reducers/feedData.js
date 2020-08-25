6125a2f4a1dac01d49c693e6d1f9fd5fcb1bedf1
Andrius <andrius.rochalazarino@gmail.com>
2020-08-25 10:41:00 -0400

    Agora reducer une feed com comentarios em um objeto só

--- src/reducers/feedData.js
+++ src/reducers/feedData.js
@@ -9,10 +9,23 @@ const FeedData = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_LOGIN_DATA': {
             return {
-                feedData: {
-                    feed: action.payload.value[0].data,
-                    comments: action.payload.value[1].data
-                }
+                // feedData: {
+                //     feed: action.payload.value[0].data,
+                //     comments: action.payload.value[1].data
+                // }
+                feedData: action.payload.value[0].data.map(
+                    feedValue=>{return{
+                        id: feedValue.id,
+                        title: feedValue.title,
+                        body: feedValue.body,
+                        imageURL: feedValue.imageURL,
+                        comments: action.payload.value[1].data.filter(
+                            commentValue=>{
+                                return commentValue.id === feedValue.id
+                            }
+                        )
+                    }}
+                )
             }
         }
         default:
